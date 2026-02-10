5d96f70bf9bd6b0fdc81c2a43e1d75ac361498f2
OisinATU <G00470518@atu.ie>
2026-02-10 09:28:20 +0000

    Created Project

---
+++ src/main/java/ie/atu/java/oopweek4lab3/OopWeek4Lab3Application.java
@@ -0,0 +1,13 @@
+package ie.atu.java.oopweek4lab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopWeek4Lab3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopWeek4Lab3Application.class, args);
+    }
+
+}
